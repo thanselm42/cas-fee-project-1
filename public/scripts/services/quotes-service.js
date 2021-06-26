70652f2e50f804da4318401cdc06bc215ce74b54
thanselm <tobias.hanselmann+github@gmail.com>
2021-06-26 15:24:43 +0200

    removed currently unused code

--- public/scripts/services/quotes-service.js
+++ public/scripts/services/quotes-service.js
@@ -1,5 +1,3 @@
-// if you like to run a local demo-version use a local quote-storage
-// import QuoteStorage from "./data/quote-storage-local.js";
 import QuoteStorage from "./data/quote-storage-remote.js";
 
 export class QuoteService {
